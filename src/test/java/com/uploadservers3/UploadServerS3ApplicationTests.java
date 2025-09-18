fa6e39623d0e8c3cb804516afa4e587a17ec510c
minseo Jo <liging12@naver.com>
2025-09-18 17:38:23 +0900

    기본 구성

---
+++ src/test/java/com/uploadservers3/UploadServerS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uploadservers3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadServerS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
